f4cd31c05ecefb2b595381e830e7aee753918b03
n8urm8 <n8urm8@mac.home>
2026-07-18 12:39:10 -0400

    more setup

---
+++ app/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./app.css";
+import Home from "./routes/home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
